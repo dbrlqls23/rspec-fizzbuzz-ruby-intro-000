75b491bf04958778476b649a952d162a679cce92
Kibin <jacobyoo00@hotmail.com>
2021-03-28 00:33:25 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,4 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz(int)
-  if int % 3 == 0
-    "Fizz"
-  end
+def fizzbuzz
 end
-
-fizzbuzz(3)
-fizzbuzz(5)
-fizzbuzz()
